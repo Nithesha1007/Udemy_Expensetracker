de3ebcaf46326bf326d161ce3fcaf083754734cd
Nithesha <nitheshanithesha841@gmail.com>
2026-08-22 07:34:59 +0530

    Expense enum

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget{
@@ -6,12 +7,26 @@ class Expenses extends StatefulWidget{
   State<Expenses>  createState() => _ExpensesState();
 }
 class _ExpensesState extends State<Expenses>{
+  final List<Expense>_registeredExpenses =[//we use the model expense here 
+         Expense(
+          title:'Flutter' , 
+          amount: 19.9,
+          date: DateTime.now(), 
+          category: Category.work
+          ),
+           Expense(
+          title:'Cinema' , 
+          amount: 15,
+          date: DateTime.now(), //dispaly current date also , we have an option using constructor 
+          category: Category.leisure
+          ),
+  ];
   @override
   Widget build(BuildContext context){
     return Scaffold(
       body: Column(
         children: [
-          Text("The Chart"),
+          Text(_registeredExpenses.toString()),
           Text("")
 
         ],
--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,14 +1,19 @@
 import 'package:uuid/uuid.dart';
 
 final uuid = Uuid();//uuid class to create an object based on it , which i will store final variable called uuid anywhere in this file we use it 
+enum Category{
+food,travel,leisure,work//here no need quotation 
+}
 class Expense {
   Expense({
   required this.title,
   required this.amount,
   required this.date,
+  required this.category//also accept the value 
   }) :id = uuid.v4();//: is initializer, v4 is to generate string unique id
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;//here no string value we mention alternatively we write what in enum that name should mention here
 }
